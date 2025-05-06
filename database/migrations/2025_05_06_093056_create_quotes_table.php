bf801bd81ae358accf1a36bc0a02d0027f09873b
Ivonne Cantor <ivonnecp@gmail.com>
2025-05-06 12:27:24 +0200

    tablas creadas

---
+++ database/migrations/2025_05_06_093056_create_quotes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', length: 20);
+            $table->text('description');
+            $table->string('site', length: 45);
+            $table->dateTime('dateTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quotes');
+    }
+};
